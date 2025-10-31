67721738576494d3ea89046389ad92352067dffa
shardulhande12-ship-it <shardulhande12@gmail.com>
2025-10-31 22:57:45 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,29 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installed');
-  self.skipWaiting();
+// ✅ Basic PWA service worker setup
+self.addEventListener('install', (event) => {
+  console.log('✅ Service Worker: Installed');
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated');
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('activate', (event) => {
+  console.log('✅ Service Worker: Activated');
 });
+
 
