96e1460784974a192f4a71347bdab63a6951c383
shardulhande12-ship-it <shardulhande12@gmail.com>
2025-10-31 22:41:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,13 @@
-// service-worker.js
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
+
